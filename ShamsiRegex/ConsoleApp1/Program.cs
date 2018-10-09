f873702dc2cb4853b07788b6214778240b5ee6c7
galiya3020 <43182182+galiya3020@users.noreply.github.com>
2018-10-09 23:17:49 +0330

    mcsd970716

---
+++ ShamsiRegex/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Please write your birth date:");
+                var pattern = @"^$|^([1۱][۰-۹ 0-9]{3}[/\/]([0 ۰][۱-۶ 1-6])[/\/]([0 ۰][۱-۹ 1-9]|[۱۲12][۰-۹ 0-9]|[3۳][01۰۱])|[1۱][۰-۹ 0-9]{3}[/\/]([۰0][۷-۹ 7-9]|[1۱][۰۱۲012])[/\/]([۰0][1-9 ۱-۹]|[12۱۲][0-9 ۰-۹]|(30|۳۰)))$";
+                Regex re = new Regex(pattern);
+                while (true)
+                {
+                    Console.Write("Date:");
+                    var text = Console.ReadLine();
+                    if (text == "exit")
+                        break;
+                    var isMatch = re.IsMatch(text);
+                    if (isMatch)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Thanks!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Not Corect!");
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+    }
+}
---
+++ punish01/ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string List = "1- students" + "\n" + "2- punishments" + "\n" + "3- go ahead";
+                Console.WriteLine(List);
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Choose an Item:");
+                var choose = Console.ReadLine();
+                if (choose == "1")
+                {
+                    Console.WriteLine("students");
+                    string[] students = new string[]
+               {
+                    "Galiya rezadoust"
+
+              };
+
+                    while (students[i]!="finish")
+                    {
+                        Console.Clear();
+                        for (int i = 0; i < students.Length; i++)
+                        {
+                            Console.WriteLine(students[i]);
+                        }
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("New Name:");
+                        
+                        var input = Console.ReadLine();
+                        AddToArray(ref students, input);
+
+                        
+
+                    }
+                }
+
+
+                else if (choose == "2")
+                {
+                    Console.WriteLine("punishments");
+                    string[] punishments = new string[]
+             {
+                    "extera practise"
+
+            };
+
+                    while (punishments[i]!= "finish")
+                    {
+                        Console.Clear();
+                        for (int i = 0; i < punishments.Length; i++)
+                        {
+                            Console.WriteLine(punishments[i]);
+                        }
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("New punishments:");
+                        var input = Console.ReadLine();
+                        AddToArray(ref punishments, input);
+
+                    }
+                }
+                else if (choose == "3")
+                {
+                    
+                    Console.WriteLine("go ahead");
+                    while (true)
+                    {
+                        //Console.WriteLine(Random studedent[i] == Random punishments[i]);
+                    }
+
+
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+
+        static void AddToArray(ref string[] names, string newName)
+        {
+            var newArray = new string[names.Length + 1];
+            for (int i = 0; i < names.Length; i++)
+            {
+                newArray[i] = names[i];
+            }
+            newArray[newArray.Length - 1] = newName;
+            names = newArray;
+        }
+
+
+    }
+
+}  
